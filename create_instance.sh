64bdfdf4adf1068e08b2ca1713547aaeea9097d9
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2018-08-23 00:57:56 +0100

    Create instance sh updated with chef-ansible switch

--- create_instance.sh
+++ create_instance.sh
@@ -4,7 +4,8 @@
 # Arg 1: instance_count: number of instance to be created
 # Arg 2: Security group name: SG rule with port 22 inbound access. It is required to connect VM using putty
 # Arg 3: Key pair name: Required for accessing the VM using putty 
-# Usage: ./create_instance.sh 1 sg_name key_name
+# Arg 4: Config mangement method specification: chef or ansible
+# Usage: ./create_instance.sh 1 sg_name key_name chef
 ###############################################
 if [ -z $1 ] ; then
   echo "Missing the instance count input param. So creating one instance"
@@ -15,6 +16,12 @@ fi
 sg_name=$2
 key_name=$3
 
+if [ $4 == 'chef' ] ; then
+cloud_init_file='cloud_init_chef.txt'
+else
+cloud_init_file='cloud_init_ansible.txt'
+fi
+echo "Cloud-init file: $cloud_init_file"
 # aws resource-groups create-group --name demo-rg$RANDOM --resource-query '{"Type":"TAG_FILTERS_1_0","Query":"{\"ResourceTypeFilters\":[\"AWS::AllSupported\"],\"TagFilters\":[{\"Key\":\"Purpose\",\"Values\":[\"Demo\"]}]}"}'
 
-aws ec2 run-instances --image-id ami-28e07e50 --count $instance_count --instance-type t2.micro --key-name $key_name --security-groups $sg_name --user-data file://install.txt --tag-specifications 'ResourceType=instance,Tags=[{Key=DEMO,Value=Instance}]'
+aws ec2 run-instances --image-id ami-28e07e50 --count $instance_count --instance-type t2.micro --key-name $key_name --security-groups $sg_name --user-data file://$cloud_init_file --tag-specifications 'ResourceType=instance,Tags=[{Key=DEMO,Value=Instance}]'
